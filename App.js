483412f9c1e967212b97f582af5fb14781a21d92
reconcubed <recongamingsam@gmail.com>
2018-06-21 14:13:25 +1000

    Formatted content into rows

--- App.js
+++ App.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   Slider,
   StyleSheet,
@@ -33,44 +33,52 @@ export default class TipCalc extends Component<Props> {
 
   splitBill(value) {
     this.state.split = value;
-    this.state.amountPerPerson = ((this.state.amountTotal + this.state.tipTotal)/value).toFixed(2);
+    this.state.amountPerPerson = ((this.state.amountTotal + this.state.tipTotal) / value).toFixed(2);
     this.setState(this.state);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Amount Total:
-        </Text>
+        <View style={styles.row}>
+          <Text>
+            Amount Total:
+          </Text>
 
-        <TextInput
-          style={styles.textInput}
-          onChangeText={this.calculateTip.bind(this)}>
-        </TextInput>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={this.calculateTip.bind(this)}>
+          </TextInput>
+        </View>
+        <View style={styles.row}>
 
-        <Text>Split Amongst: {this.state.split}</Text>
+          <Text>Split Amongst: {this.state.split}</Text>
 
-        <Slider
-          maximumValue={20}
-          minimumValue={1}
-          step={1}
-          value={this.state.split}
-          onValueChange={this.splitBill.bind(this)}
-          style={styles.slider}
+          <Slider
+            maximumValue={20}
+            minimumValue={1}
+            step={1}
+            value={this.state.split}
+            onValueChange={this.splitBill.bind(this)}
+            style={styles.slider}
           />
+        </View>
+        <View style={styles.row}>
+          <Text>Amount Per Person:</Text>
+          <Text style={styles.amount}>
+            ${this.state.amountPerPerson}
+          </Text>
+        </View>
+        <View style={styles.row}>
 
-        <Text>Amount Per Person:</Text>
-        <Text style={styles.amount}>
-          ${this.state.amountPerPerson}
-        </Text>
-        <Text>
-          Total Tip:
-        </Text>
+          <Text>
+            Total Tip:
+          </Text>
 
-        <Text style={styles.amount}>
-          ${this.state.tipTotal.toFixed(2)}
-        </Text>
+          <Text style={styles.amount}>
+            ${this.state.tipTotal.toFixed(2)}
+          </Text>
+        </View>
       </View>
     );
   }
@@ -79,16 +87,22 @@ export default class TipCalc extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  row: {
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    height: 50,
   },
   textInput: {
     textAlign: 'left',
     color: '#333333',
     margin: 5,
     height: 50,
-    width: 300,
+    width: 200,
     borderColor: '#60b7e2',
     fontSize: 30,
     borderWidth: 1
@@ -100,6 +114,6 @@ const styles = StyleSheet.create({
   slider: {
     margin: 5,
     height: 40,
-    width: 200,
+    width: 170,
   }
 });
